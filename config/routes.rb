769e3f2fa64b061128289927864b3a02ebc66f80
Kian Ting <kian.ting@outlook.com>
2017-09-20 20:46:11 +1200

    added few more pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/contact'
+
+  get 'home/faq'
+
   root 'home#index'
 
+  get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
