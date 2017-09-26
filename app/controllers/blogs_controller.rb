1e5ebf31c85a6bd8353d9d079e577eb5fadfc2ee
Kian Ting <kian.ting@outlook.com>
2017-09-27 00:44:13 +1300

    modified the new blogs form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -71,7 +71,7 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :blog, :entry)
+      params.require(:blog).permit(:title, :blog, :entry, :image)
     end
     
     def correct_user 
