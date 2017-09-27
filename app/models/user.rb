02416c76a8e4eb320a2988f1cabf010d5b0db7e2
Kian Ting <kian.ting@outlook.com>
2017-09-27 23:44:31 +1300

    Added constraint for unique user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of :name
   has_many :blogs
 end
