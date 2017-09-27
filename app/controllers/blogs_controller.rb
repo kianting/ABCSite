8770fb7a4d22d3233db99b721862e9d07157e4b1
Kian Ting <kian.ting@outlook.com>
2017-09-27 23:15:26 +1300

    modified to add user name

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -71,7 +71,7 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :blog, :entry, :image)
+      params.require(:blog).permit(:title, :blog, :entry, :image, :name)
     end
     
     def correct_user 
